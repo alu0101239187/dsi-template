e965425b36dcfd058057e6be68c122f7ba440ae7
Daniel Jorge Acosta <alu0101239187@ull.edu.es>
2023-03-22 18:31:48 +0000

    Added test files

---
+++ src/test.ts
@@ -0,0 +1,14 @@
+/**
+ * Adds two __numbers__
+ * @param firstNumber Consists of the first operand of the addition
+ * @param secondNumber Consists of the second operand of the addition
+ * @returns The addition of the two numbers `firstNumber` and `secondNumber`
+ * ```typescript
+ * add(1, 7) = 8
+ * ```
+ */
+export function add(firstNumber: number, secondNumber: number) {
+  return firstNumber + secondNumber;
+}
+
+console.log(`add(1, 7): ${add(1, 7)}`);
